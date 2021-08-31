92d95f639d3188365f1b7864322380c96ef447ec
Taiji-Boy <Dao-prog@yandex.ru>
2021-08-31 15:40:34 +0300

    Добавил setup.py для cx_freeze

---
+++ setup.py
@@ -0,0 +1,32 @@
+# coding: utf-8
+
+from cx_Freeze import setup, Executable
+
+executables = [Executable('main.py',
+                          targetName='KompasMass.exe',
+                          base='Win32GUI',
+                          icon='icon.ico')
+              ]
+
+excludes = ['logging', 'unittest', 'email', 'html', 'http', 'urllib', 'xml',
+            'unicodedata', 'bz2', 'select']
+
+include_files = ['config.ini', 'tkinterWind.py']
+
+zip_include_packages = ['collections', 'encodings', 'importlib']
+
+options = {
+    'build_exe': {
+        'include_msvcr': True,
+        'excludes': excludes,
+        'zip_include_packages': zip_include_packages,
+        'build_exe': 'build_windows',
+        'include_files': include_files,
+    }
+}
+
+setup(name='KompasMass',
+      version='1.0',
+      description='Kompas app',
+      executables=executables,
+      options=options)
